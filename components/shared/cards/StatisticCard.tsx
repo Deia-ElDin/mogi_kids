5a197b8c160ed910559b154ecab62896852a6a45
Deiaa Eldin Hamad <dehamad@lab3r8s3.42abudhabi.ae>
2024-02-17 01:18:36 +0400

    updaing cards folder name

---
+++ components/shared/cards/StatisticCard.tsx
@@ -0,0 +1,35 @@
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import Image from "next/image";
+
+type StatisticCardParams = {
+  statistic: {
+    icon: string;
+    title: string;
+    rating: string;
+    color: string;
+  };
+};
+
+const StatisticCard = ({ statistic }: StatisticCardParams) => {
+  return (
+    <Card
+      className="flex flex-col min-w-[200px] h-[200px] justify-around items-center border rounded-lg p-3"
+      style={{ backgroundColor: statistic.color }}
+    >
+      <CardContent>
+        <Image
+          src={statistic.icon}
+          alt={statistic.title}
+          height={50}
+          width={70}
+        />
+      </CardContent>
+      <CardFooter className="flex flex-col items-center gap-3 w-full">
+        <p className="text-yellow-800 text-2xl font-bold">{statistic.rating}</p>
+        <p className="txt-lg font-bold">{statistic.title}</p>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default StatisticCard;
