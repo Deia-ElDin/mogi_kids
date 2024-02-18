8df7a83353e2b73d5db17119cf9ff73b0c641ca0
Deiaa Eldin Hamad <dehamad@lab3r8s3.42abudhabi.ae>
2024-02-18 17:56:27 +0400

    fixing the utapi imports

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -1,10 +1,12 @@
 import { generateReactHelpers } from "@uploadthing/react/hooks";
 
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
-
 import { UTApi } from "uploadthing/server";
-
-export const utapi = new UTApi();
 
 export const { useUploadThing, uploadFiles } =
   generateReactHelpers<OurFileRouter>();
+
+export const utapi = new UTApi({
+  fetch: globalThis.fetch,
+  apiKey: process.env.UPLOADTHING_SECRET,
+});
