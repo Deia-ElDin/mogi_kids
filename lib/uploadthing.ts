78ca7f26e1b672873169fb428c8838ea0c17e502
Deiaa Eldin Hamad <dehamad@lab3r8s3.42abudhabi.ae>
2024-02-18 16:50:32 +0400

    double check on vercal with uploadthing

--- lib/uploadthing.ts
+++ lib/uploadthing.ts
@@ -2,7 +2,9 @@ import { generateReactHelpers } from "@uploadthing/react/hooks";
 
 import type { OurFileRouter } from "@/app/api/uploadthing/core";
 
+import { UTApi } from "uploadthing/server";
+
+export const utapi = new UTApi();
+
 export const { useUploadThing, uploadFiles } =
   generateReactHelpers<OurFileRouter>();
-
-
