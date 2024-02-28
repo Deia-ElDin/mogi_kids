16d9590999f993c6a9f942b5f90b854eb1e3cdc8
Deia Hamad <deia.tech2021@gmail.com>
2024-02-28 20:51:46 +0400

    fixing the star base color

--- components/shared/helpers/RatingInput.tsx
+++ components/shared/helpers/RatingInput.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Dispatch, SetStateAction } from "react";
+import { usePathname } from "next/navigation";
 import { FaStar } from "react-icons/fa";
 
 type RatingProps = {
@@ -9,6 +10,8 @@ type RatingProps = {
 };
 
 const RatingInput = ({ rating, setRating }: RatingProps) => {
+  const pathname = usePathname();
+
   const handleClick = (value: number) => {
     setRating(value);
   };
@@ -17,11 +20,17 @@ const RatingInput = ({ rating, setRating }: RatingProps) => {
     <div className="flex gap-2">
       {[...Array(5)].map((_, index) => {
         const starValue = index + 1;
+        console.log("starValue = ", starValue);
+        console.log("rating = ", rating);
         return (
           <FaStar
             key={index}
-            className={`text-white w-[30px] h-[30px] cursor-pointer  ${
-              starValue <= rating ? "text-yellow-500" : "text-gray-900"
+            className={`w-[30px] h-[30px] cursor-pointer  ${
+              starValue <= rating
+                ? "text-yellow-500"
+                : pathname === "/"
+                ? "text-gray-900"
+                : "text-gray-300"
             }`}
             onClick={() => handleClick(starValue)}
           />
