997c064edfbb8deb32d9c2f14b75f1e08051d149
Deia Hamad <deia.tech2021@gmail.com>
2024-03-03 12:04:43 +0400

    vercel 9th

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,29 +1,51 @@
-import { EmailTemplate, EmailTemplateProps } from "@/components/email-template";
+import { EmailTemplate } from "@/components/email-template";
 import { Resend } from "resend";
-import { createQuote } from "@/lib/actions/quote.actions";
-import { NextResponse } from "next/server";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(props: any) {
-  console.log("props", props);
-
   try {
-    const { data, error } = await resend.emails.send({
-      from: "Resend Email Service <onboarding@resend.dev>",
-      to: ["it.alqabda@gmail.com"],
-      subject: `Quotation - ${props.cstName ?? "Unknown"}`,
-      react: EmailTemplate({ ...props }) as React.ReactElement,
+    const data = await resend.emails.send({
+      from: "Acme <onboarding@resend.dev>",
+      to: ["delivered@resend.dev"],
+      subject: "Hello world",
+      react: EmailTemplate(props) as React.ReactElement,
     });
-    if (error) {
-      return new NextResponse(JSON.stringify({ error }), { status: 500 });
-    }
 
-    return new NextResponse(JSON.stringify({ data }), { status: 200 });
+    return Response.json(data);
   } catch (error) {
-    return new NextResponse(JSON.stringify({ error }), { status: 500 });
+    return Response.json({ error });
   }
 }
+
+// "use server";
+
+// import { EmailTemplate, EmailTemplateProps } from "@/components/email-template";
+// import { Resend } from "resend";
+// import { createQuote } from "@/lib/actions/quote.actions";
+// import { NextResponse } from "next/server";
+
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+// export async function POST(props: any) {
+//   console.log("props", props);
+
+//   try {
+//     const { data, error } = await resend.emails.send({
+//       from: "Resend Email Service <onboarding@resend.dev>",
+//       to: ["it.alqabda@gmail.com"],
+//       subject: `Quotation - ${props.cstName ?? "Unknown"}`,
+//       react: EmailTemplate({ ...props }) as React.ReactElement,
+//     });
+//     if (error) {
+//       return new NextResponse(JSON.stringify({ error }), { status: 500 });
+//     }
+
+//     return new NextResponse(JSON.stringify({ data }), { status: 200 });
+//   } catch (error) {
+//     return new NextResponse(JSON.stringify({ error }), { status: 500 });
+//   }
+// }
 
 // export async function sendEmail(props: EmailTemplateProps) {
 //   try {
